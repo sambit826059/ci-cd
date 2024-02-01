a4848ba40151677a7fc70f072eef2c1843881e74
Sambit Mohanty <sambit@sambit.domain.name>
2024-02-01 09:25:02 +0530

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, add_task])
+}
+
+use std::{fs::{OpenOptions}, io::{Write}};
+use rocket::serde::{Deserialize, json::Json};
+
+#[derive(Deserialize, Serialize)]
+#[serde(crate = "rocket::serde")]
+struct Task<'r> {
+    item: &'r str
+}
+
+#[post("/addtask", data="<task>")]
+fn add_task(task: Json<Task<'_>>) -> &'static str {
+    let mut tasks = OpenOptions::new()
+                    .read(true)
+                    .append(true)
+                    .create(true)
+                    .open("tasks.txt")
+                    .expect("unable to access tasks.txt");   
+    let task_item_string = format!("{}\n", task.item);
+    let task_item_bytes = task_item_string.as_bytes();
+    tasks.write(task_item_bytes).expect("unable to write to tasks.txt");
+    "Task added succesfully"
+}
+use std::io::{self, BufRead, BufReader};
+
+#[get("/readtasks")]
+fn read_tasks() -> Json<Vec<String>> {
+    let tasks = OpenOptions::new()
+                    .read(true)
+                    .append(true)
+                    .create(true)
+                    .open("tasks.txt")
+                    .expect("unable to access tasks.txt");  
+    let reader = BufReader::new(tasks);
+    Json(reader.lines()
+            .map(|line| line.expect("could not read line"))
+            .collect())
+}
